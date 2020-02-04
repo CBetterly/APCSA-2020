554871b8a1c313182e9e45bfcfc5fa4e29a98580
betterlyc3524 <betterlyc3524@CA-SU-F106-13.sduhsd.lan>
2020-02-04 10:51:48 -0800

    2/4: updated formatting

--- APCS-A_2020/src/unit2/Cube.java
+++ APCS-A_2020/src/unit2/Cube.java
@@ -1,13 +1,13 @@
 package unit2;
 //(c) A+ Computer Science
+
 // www.apluscompsci.com
 //Name -
 //Date -
 
-public class Cube
-{
-	public static double area( int side )
-	{
-		return side*side;
+public class Cube {
+	public static double area(int side) {
+		//assuming area means the area of the base of the cube; we would use volume if we wanted volume
+		return side * side;
 	}
 }
--- APCS-A_2020/src/unit2/Trap.java
+++ APCS-A_2020/src/unit2/Trap.java
@@ -1,14 +1,13 @@
 package unit2;
 //© A+ Computer Science  -  www.apluscompsci.com
+
 //Name -
 //Date -
 //Class -
 //Lab  - 
 
-public class Trap
-{
-	public static double area( int base1, int base2, int height )
-	{
-		return (base1+base2)/2*height;
+public class Trap {
+	public static double area(int base1, int base2, int height) {
+		return (base1 + base2) / 2 * height;
 	}
 }
