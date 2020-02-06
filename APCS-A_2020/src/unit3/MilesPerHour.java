bf7561b345ad57112f1be8aba85bd7ca9f715b6b
betterlyc3524 <betterlyc3524@CA-SU-F106-13.sduhsd.lan>
2020-02-06 11:12:48 -0800

    MilesPerHour - 2/6/20

--- APCS-A_2020/src/unit3/MilesPerHour.java
+++ APCS-A_2020/src/unit3/MilesPerHour.java
@@ -24,24 +24,30 @@ public class MilesPerHour
 
 	public MilesPerHour(int dist, int hrs, int mins)
 	{
+		this.setNums(dist, hrs, mins);
 	}
 
 	public void setNums(int dist, int hrs, int mins)
 	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
 	}
 
 	public void calcMPH()
 	{
+		mph = distance / (hours + minutes%60/60.0);
 	}
 
 	public void print()
 	{
+		System.out.println("MPH: " + String.format("%.5f", mph));
 	}
 	
 	//create a print or toString or both
 	
 	public String toString()
 	{
-		return "";
+		return "toString in progress";
 	}
 }
