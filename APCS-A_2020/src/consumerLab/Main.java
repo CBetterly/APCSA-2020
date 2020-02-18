09ee61cd1d7ad5a0185e668105202fadacadea44
betterlyc3524 <betterlyc3524@CA-SU-F106-13.sduhsd.lan>
2020-02-18 11:11:55 -0800

    from class 2/18

--- APCS-A_2020/src/consumerLab/Main.java
+++ APCS-A_2020/src/consumerLab/Main.java
@@ -3,21 +3,23 @@ package consumerLab;
 import java.io.File;
 import java.util.Scanner;
 
-
 class Main {
 	public static void main(String[] args) {
 		// test reading from files
 		// read in the positive adjectives in postiveAdjectives.txt
-		try {
-			Scanner input = new Scanner(new File("src/consumerLab/positiveAdjectives.txt"));
-			while (input.hasNextLine()) {
-				String temp = input.nextLine().trim();
-				System.out.println(temp);
-
-			}
-			input.close();
-		} catch (Exception e) {
-			System.out.println("Error reading or parsing postitiveAdjectives.txt\n" + e);
-		}
+		// System.out.println(Review.sentimentVal("epic"));
+		System.out.println(Review.totalSentiment("src/consumerLab/simpleReview.txt"));
+		System.out.println("This review gives... " + Review.starRating("src/consumerLab/simpleReview.txt") + " star(s)!");
+		System.out.println(Review.totalSentiment("src/consumerLab/26WestReview.txt"));
+		System.out.println("This review gives... " + Review.starRating("src/consumerLab/26WestReview.txt") + " star(s)!");
+		/*
+		 * try { Scanner input = new Scanner(new
+		 * File("src/consumerLab/positiveAdjectives.txt")); while (input.hasNextLine())
+		 * { String temp = input.nextLine().trim(); System.out.println(temp);
+		 * 
+		 * } input.close(); } catch (Exception e) {
+		 * System.out.println("Error reading or parsing postitiveAdjectives.txt\n" + e);
+		 * }
+		 */
 	}
 }
