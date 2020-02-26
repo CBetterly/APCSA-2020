3699df187c09d3b970eb2a51215f637d621e08bb
Connor Betterly <Connor Betterly@CONNORS-PREDATOR-G3-710>
2020-02-26 15:48:49 -0800

    from home 2/26

--- APCS-A_2020/src/unit9/DownRunner.java
+++ APCS-A_2020/src/unit9/DownRunner.java
@@ -1,12 +1,38 @@
 package unit9;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 //Date -
 
-public class DownRunner
-{
-	public static void main( String args[] )
-	{											
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DownRunner {
+	public static void main(String args[]) {
+		ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(-99, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12345));
+		System.out.println(ListDown.go(a));
+		ArrayList<Integer> b = new ArrayList<Integer>(Arrays.asList(10,9,8,7,6,5,4,3,2,1,-99));
+		System.out.println(ListDown.go(b));
+		ArrayList<Integer> c = new ArrayList<Integer>(Arrays.asList(10,20,30,40,50,-11818,40,30,20,10));
+		System.out.println(ListDown.go(c));
+		ArrayList<Integer> d = new ArrayList<Integer>(Arrays.asList(32767));
+		System.out.println(ListDown.go(d));
+		ArrayList<Integer> e = new ArrayList<Integer>(Arrays.asList(255,255));
+		System.out.println(ListDown.go(e));
+		ArrayList<Integer> f = new ArrayList<Integer>(Arrays.asList(9,10,-88,100,-555,1000));
+		System.out.println(ListDown.go(f));
+		ArrayList<Integer> g = new ArrayList<Integer>(Arrays.asList(10,10,10,11,456));
+		System.out.println(ListDown.go(g));
+		ArrayList<Integer> h = new ArrayList<Integer>(Arrays.asList(-111,1,2,3,9,11,20,30));
+		System.out.println(ListDown.go(h));
+		ArrayList<Integer> i = new ArrayList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,0,-2,-989));
+		System.out.println(ListDown.go(i));
+		ArrayList<Integer> j = new ArrayList<Integer>(Arrays.asList(12,15,18,21,23,1000));
+		System.out.println(ListDown.go(j));
+		ArrayList<Integer> k = new ArrayList<Integer>(Arrays.asList(250, 19, 17, 15, 13, 11, 10, 9, 6, 3, 2, 1, -455));
+		System.out.println(ListDown.go(k));
+		ArrayList<Integer> l = new ArrayList<Integer>(Arrays.asList(9,10,-8,10000,-5000,1000));
+		System.out.println(ListDown.go(l));
 	}
 }
--- APCS-A_2020/src/unit9/ListOddToEven.java
+++ APCS-A_2020/src/unit9/ListOddToEven.java
@@ -11,8 +11,32 @@ import java.util.List;
 public class ListOddToEven {
 
 	public static int returnDist(ArrayList<Integer> arrayPassed) {
-
-		return 0;
+		int firstOdd = -1;
+		int lastEven = -1;
+		for(int i = 0; i < arrayPassed.size(); i++) {
+			if(arrayPassed.get(i) % 2 == 1) {
+				firstOdd = i;
+				break;
+			}
+		}
+		
+		if(firstOdd == -1) {
+			return -1;
+		}
+		
+		for(int i = arrayPassed.size()-1; i > 0; i--) {
+			if(arrayPassed.get(i) % 2 == 0) {
+				lastEven = i;
+				break;
+			}
+		}
+		
+		if(firstOdd>lastEven) {
+			return -1;
+		}
+			
+		
+		return lastEven-firstOdd;
 	}
 
 	public static boolean go(List<Integer> ray) {
