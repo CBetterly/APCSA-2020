f11d72cf71ed2d26f4f877842a68b930afd0eb92
Connor Betterly <Connor Betterly@CONNORS-PREDATOR-G3-710>
2020-03-06 22:01:41 -0800

    from home 3/6

--- APCS-A_2020/src/unit11/Rational.java
+++ APCS-A_2020/src/unit11/Rational.java
@@ -1,92 +1,113 @@
 package unit11;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
-import static java.lang.System.*; 
+import static java.lang.System.*;
 
-class Rational implements Comparable<Rational>
-{
-	//add two instance variables
+class Rational implements Comparable<Rational> {
+	// add two instance variables
 	private int numerator;
 	private int denominator;
 
-	//write two constructors
+	// write two constructors
 	public Rational() {
 		numerator = 1;
 		denominator = 2;
 	}
-	
+
 	public Rational(int num, int denom) {
 		numerator = num;
 		denominator = denom;
 	}
 
-	//write a setRational method
-	
+	// write a setRational method
+
 	public void setRational(int num, int denom) {
 		numerator = num;
 		denominator = denom;
 	}
 
-	//write  a set method for numerator and denominator
+	// write a set method for numerator and denominator
 
-	
-	public void add(Rational  other)
-	{
-		//num1/den1 + num2/den2 
-		//new numerator = (num1 * den2 + num2 * den1)
-		//new denominator = (den1 * den2)
-		
-		
+	public void add(Rational other) {
+		// num1/den1 + num2/den2
+		// new numerator = (num1 * den2 + num2 * den1)
+		// new denominator = (den1 * den2)
 
+		numerator = numerator * other.getDenominator() + other.getNumerator() * denominator;
+		denominator = denominator * other.getDenominator();
 
 		reduce();
 	}
 
-	private void reduce()
-	{
-
-
+	private void reduce() {
+		int gcd = gcd(numerator, denominator);
+		while (gcd != 1) {
+			numerator = numerator/gcd;
+			denominator = denominator/gcd;
+			gcd = gcd(numerator, denominator);
+		}
 
 	}
 
-	private int gcd(int numOne, int numTwo)
-	{
+	private int gcd(int numOne, int numTwo) {
 
+		int gcd = 1;
 
-		return 1;
+		for (int i = 1; i <= numOne && i <= numTwo; i++) {
+			if (numOne % i == 0 && numTwo % i == 0)
+				gcd = i;
+		}
+
+		return gcd;
 	}
 
-	public Object clone ()
-	{
-		return "";
+	public Rational clone() {
+		return this;
 	}
 
+	// ACCESSORS
 
-	//ACCESSORS
+	// write get methods for numerator and denominator
+	public int getNumerator() {
+		return numerator;
+	}
 
-	//write get methods for numerator and denominator
-	
-	
-	public boolean equals( Object obj)
-	{
+	public int getDenominator() {
+		return denominator;
+	}
 
+	public void setNumerator(int newNumerator) {
+		numerator = newNumerator;
+	}
 
+	public void setDenominator(int newDenominator) {
+		denominator = newDenominator;
+	}
+
+	public boolean equals(Rational obj) {
+		if(((double)numerator)/denominator == ((double) obj.getNumerator()/obj.getDenominator())) {
+			return true;
+		}
 		return false;
 	}
 
-	public int compareTo(Rational other)
-	{
-
+	public int compareTo(Rational other) {
+		if(this.equals(other)) {
+			return 0;
+		}
+		if(((double)numerator)/denominator >= ((double) other.getNumerator()/other.getDenominator())) {
+			return 1;
+		}
 
 		return -1;
 	}
 
+	// write toString() method
+	public String toString() {
+		return numerator + "/" + denominator;
+	}
 
-
-	
-	//write  toString() method
-	
-	
 }
